91f2f90a0bed3c5615a9f798123fd348426216d1
Jérémy Buget <jbuget@octo.com>
2017-02-13 18:21:04 +0100

    Improve code quality

--- app/components/message-input.js
+++ app/components/message-input.js
@@ -2,11 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-  store: Ember.inject.service(),
-
   actions: {
     submitMessage() {
-      this.sendAction('onSubmit', this.get('content'));
+      this.sendAction('submitHandler', this.get('content'));
       this.set('content', null);
     }
   }
