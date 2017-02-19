142899798f429a61461095e5531afa8b08fcf8ff
Jérémy Buget <jbuget@octo.com>
2017-02-19 20:47:53 +0100

    Try fo fix GitHub Pages deployment

--- config/environment.js
+++ config/environment.js
@@ -46,7 +46,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     ENV.locationType = 'hash';
-    ENV.rootURL = 'microblog-ember';
+    ENV.rootURL = '/microblog-ember/';
     ENV.APP.apiHost = 'https://microblog-api.herokuapp.com';
   }
 
