3e6bc06b1e33ef8fa37f9d512bce3c9bb54a6496
Jérémy Buget <jbuget@octo.com>
2017-02-19 17:45:00 +0100

    Add styles (+ font-awesome)

--- app/components/message-list.js
+++ app/components/message-list.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 const MessageList = Ember.Component.extend({
 
+  hasNoMessage: Ember.computed.empty('messages'),
+
   reverseMessages: Ember.computed('messages.[]', function () {
     return this.get('messages').toArray().reverse();
   })
